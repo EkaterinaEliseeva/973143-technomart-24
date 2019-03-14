c891e804964fdaf65b422e40bd50d43a3fa20224
Ekaterina <45285300+EkaterinaEliseeva@users.noreply.github.com>
2019-03-14 23:41:26 +0300

    Изменения

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 var link = document.querySelector(".company-contacts-button");
 var popup = document.querySelector(".modal-write-us");
-var closebutton = document.querySelector(".modal-write-us-close");
-var formwriteus = document.querySelector(".modal-write-us-form");
-var inputname = document.querySelector(".input-name");
-var inputemail = document.querySelector(".input-email");
-var inputtext = document.querySelector(".modal-write-us-textarea");
+var closeButton = document.querySelector(".modal-write-us-close");
+var formWriteUs = document.querySelector(".modal-write-us-form");
+var inputName = document.querySelector(".input-name");
+var inputEmail = document.querySelector(".input-email");
+var inputText = document.querySelector(".modal-write-us-textarea");
 var isStorageSupport = true;
-var storagename = "";
-var storageemail = "";
+var storageName = "";
+var storageEmail = "";
 var modal = document.querySelectorAll(".modal");
 
 try {
-    storagename = localStorage.getItem("inputname");
-    storageemail = localStorage.getItem("inputemail");
+    storageName = localStorage.getItem("inputName");
+    storageEmail = localStorage.getItem("inputEmail");
 } catch (err) {
     isStorageSupport = false;
 }
@@ -21,35 +21,35 @@ if (link) link.addEventListener("click", function(evt) {
     evt.preventDefault(); 
     popup.classList.add("modal-show");
     
-    if (storagename && storageemail)  {
-        inputname.value = storagename;
-        inputname.email = storageemail;
-        inputtext.focus();
+    if (storageName && storageEmail)  {
+        inputName.value = storageName;
+        inputEmail.value = storageEmail;
+        inputText.focus();
     } else {
-        if (storagename && !storageemail) {
-            inputemail.focus();
+        if (storageName && !storageEmail) {
+            inputEmail.focus();
         } else {
-            inputname.focus();
+            inputName.focus();
         }   
     }       
 });
 
-if (closebutton) closebutton.addEventListener("click", function(evt) {
+if (closeButton) closeButton.addEventListener("click", function(evt) {
     evt.preventDefault(); 
     popup.classList.remove("modal-show");
     popup.classList.remove("modal-error");
 });
 
-if (formwriteus) formwriteus.addEventListener("submit", function(evt) {
-    if (!inputname.value || !inputemail.value || !inputtext.value) {
+if (formWriteUs) formWriteUs.addEventListener("submit", function(evt) {
+    if (!inputName.value || !inputEmail.value || !inputText.value) {
         evt.preventDefault();
         popup.classList.remove("modal-error");
         popup.offsetWidth = popup.offsetWidth;
         popup.classList.add("modal-error");
     } else {
         if (isStorageSupport) {
-            localStorage.setItem("inputname", inputname.value);
-            localStorage.setItem("inputemail", inputemail.value);
+            localStorage.setItem("inputName", inputname.value);
+            localStorage.setItem("inputEmail", inputemail.value);
         }
     }
 });
@@ -66,33 +66,37 @@ window.addEventListener("keydown", function (evt) {
 });
 
 
-var maplink = document.querySelector(".company-contacts-map");
-var mappopup = document.querySelector(".modal-map");
-var mapclose = document.querySelector(".modal-map-close");
+var mapLink = document.querySelector(".company-contacts-map");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = document.querySelector(".modal-map-close");
+var mapIframe = document.querySelector(".modal-map-iframe");
 
-if (maplink) maplink.addEventListener("click", function(evt) {
+if (mapLink) mapLink.addEventListener("click", function(evt) {
     evt.preventDefault();
-    mappopup.classList.add("modal-show");
+    mapPopup.classList.add("modal-show");
+    mapPopup.classList.remove("modal-map-img");
+    mapIframe.classList.remove("visually-hidden");
 });
 
-if (mapclose) mapclose.addEventListener("click", function(evt) {
+if (mapClose) mapClose.addEventListener("click", function(evt) {
     evt.preventDefault();
-    mappopup.classList.remove("modal-show");
+    mapPopup.classList.remove("modal-show");
+    mapPopup.classList.add("modal-map-img");
 });
 
 
-var cartlink = document.querySelectorAll(".catalog-item-action-buy");
-var cartpopup = document.querySelector(".modal-cart");
-var cartclose = document.querySelector(".modal-cart-close");
+var cartLink = document.querySelectorAll(".catalog-item-action-buy");
+var cartPopup = document.querySelector(".modal-cart");
+var cartClose = document.querySelector(".modal-cart-close");
 
-if (cartlink) for (var i=0; i<cartlink.length; i++) {
-    cartlink[i].addEventListener("click", function(evt) {
+if (cartLink) for (var i=0; i<cartLink.length; i++) {
+    cartLink[i].addEventListener("click", function(evt) {
         evt.preventDefault();
-        cartpopup.classList.add("modal-show");
+        cartPopup.classList.add("modal-show");
     });
 };
 
-if (cartclose) cartclose.addEventListener("click", function(evt) {
+if (cartClose) cartClose.addEventListener("click", function(evt) {
 evt.preventDefault();
-cartpopup.classList.remove("modal-show");
+cartPopup.classList.remove("modal-show");
 });
